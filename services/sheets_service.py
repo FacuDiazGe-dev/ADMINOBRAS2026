077d05777a7419d4ba0640f7e711ae5808844f2c
FacuDiazGe-dev <facundodiazgramajo@gmail.com>
2026-05-08 19:55:21 -0300

    Update sheets_service.py

--- services/sheets_service.py
+++ services/sheets_service.py
@@ -1,28 +1,37 @@
+import streamlit as st
 import gspread
-from google.oauth2.service_account import Credentials
 import pandas as pd
+
+from google.oauth2.service_account import Credentials
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-CREDS = Credentials.from_service_account_file(
-    "data/credentials.json",
+creds_dict = dict(st.secrets["gcp_service_account"])
+
+CREDS = Credentials.from_service_account_info(
+    creds_dict,
     scopes=SCOPES
 )
 
 client = gspread.authorize(CREDS)
 
-SPREADSHEET = client.open("AdminObrasDB")
+SPREADSHEET = client.open("ADMINISTRADOROBRA2026")
 
 
 def get_dataframe(sheet_name):
+
     sheet = SPREADSHEET.worksheet(sheet_name)
+
     data = sheet.get_all_records()
+
     return pd.DataFrame(data)
 
 
 def append_row(sheet_name, row):
+
     sheet = SPREADSHEET.worksheet(sheet_name)
+
     sheet.append_row(row)
